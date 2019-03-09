4906088b2f3afc6661aefdaa4a7b09f72007bccd
rorymcc1 <rory.mccrave@sac-it.us>
2019-03-08 20:10:27 -0500

    Remove sss

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
   <div id="app">
     <h1>Test Resultsxxxxxxxxxxxxxxxx</h1>
     <button v-if="panels.length > 0" class="btn btn-info" v-on:click="refreshData">
-      Refresh Listssssssss
+      Refresh List
     </button>
     <button v-if="panels.length == 0" class="btn btn-success" v-on:click="refreshData">
-      Generate Listssssssss
+      Generate List
     </button>
     <div class="card" v-for="panel in panels">
       <div class="card-header bg-info">
